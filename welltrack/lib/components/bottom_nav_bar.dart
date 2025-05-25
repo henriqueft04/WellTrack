cb6c2cda0ddcdbc43da14b5518e4c5633744495d
carloslf2258 <carlos.verenzuela@ua.pt>
2025-05-25 14:43:17 +0100

    Mudança estética navBar

--- welltrack/lib/components/bottom_nav_bar.dart
+++ welltrack/lib/components/bottom_nav_bar.dart
@@ -8,18 +8,23 @@ class MyBottomNavBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      color: const Color.fromARGB(255, 30, 173, 245), // Cor de fundo
       //Padding between buttons
       padding: EdgeInsets.symmetric(vertical: 20),
       child: GNav(
         //decoration (colors, activecolors and borders)
-        color: Colors.grey[500],
-        activeColor: Colors.grey.shade800,
-        tabActiveBorder: Border.all(color: Colors.white),
-        tabBackgroundColor: Colors.grey.shade200,
+        color: const Color.fromARGB(255, 0, 0, 0),
+        activeColor: const Color(0xFF2799FF),
+        tabActiveBorder: Border.all(
+          color: const Color.fromARGB(255, 101, 114, 236),
+        ),
+
+        // Background color of the active tab
+        tabBackgroundColor: const Color(0xFFCDEDFD),
         mainAxisAlignment: MainAxisAlignment.center,
         tabBorderRadius: 16,
 
-      onTabChange: (value) => onTabChange!(value) ,
+        onTabChange: (value) => onTabChange!(value),
 
         tabs: const [
           //buttons
@@ -27,7 +32,6 @@ class MyBottomNavBar extends StatelessWidget {
           GButton(icon: Icons.calendar_month, text: 'Calendar'),
           GButton(icon: Icons.stacked_bar_chart_sharp, text: 'Stats'),
           GButton(icon: Icons.person, text: 'Profile'),
-
         ],
       ),
     );
